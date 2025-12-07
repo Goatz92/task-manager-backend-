216526ee3b7150a3c634d3a8b12ecfd53beeebd5
Goatz <Goataragua@gmail.com>
2025-12-07 16:47:17 +0200

    Create Auth Req, Res

---
+++ src/main/java/com/goatz/task_manager_pro/dto/AuthRequest.java
@@ -0,0 +1,15 @@
+package com.goatz.task_manager_pro.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class AuthRequest {
+    private String username;
+    private String password;
+}
