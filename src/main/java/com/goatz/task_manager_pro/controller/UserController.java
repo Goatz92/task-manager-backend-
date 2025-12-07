86576dcf90ad64dba13ff29d71dbada362ec09d8
Goatz <Goataragua@gmail.com>
2025-12-07 16:25:25 +0200

    Create UserController for basic CRUD ops

--- src/main/java/com/goatz/task_manager_pro/controller/UserController.java
+++ src/main/java/com/goatz/task_manager_pro/controller/UserController.java
@@ -1,4 +1,32 @@
 package com.goatz.task_manager_pro.controller;
 
+import com.goatz.task_manager_pro.model.User;
+import com.goatz.task_manager_pro.service.UserService;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/user")
+@RequiredArgsConstructor
 public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userService.createUser(user);
+    }
+
+    @GetMapping
+    public List<User> getAllUsers(){
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public User getUserById (@PathVariable Integer id) {
+        return userService.getUserById(id);
+    }
 }
