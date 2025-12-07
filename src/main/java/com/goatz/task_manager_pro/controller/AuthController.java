2855a19964808310939ea0d6c850fc9521593e08
Goatz <Goataragua@gmail.com>
2025-12-07 17:40:52 +0200

    Create AuthController to handle auth req/res

--- src/main/java/com/goatz/task_manager_pro/controller/AuthController.java
+++ src/main/java/com/goatz/task_manager_pro/controller/AuthController.java
@@ -1,4 +1,28 @@
 package com.goatz.task_manager_pro.controller;
 
+import com.goatz.task_manager_pro.dto.AuthRequest;
+import com.goatz.task_manager_pro.dto.AuthResponse;
+import com.goatz.task_manager_pro.service.AuthService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/auth")
 public class AuthController {
+
+    private final AuthService authService;
+
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(@RequestBody AuthRequest request) {
+        AuthResponse response = authService.authenticate(request);
+        return ResponseEntity.ok(response);
+    }
 }
