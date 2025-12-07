d831ed64111281e59387594d7f7e69f41232297d
Goatz <Goataragua@gmail.com>
2025-12-07 16:28:38 +0200

    Add passwordEncoder

--- src/main/java/com/goatz/task_manager_pro/service/UserService.java
+++ src/main/java/com/goatz/task_manager_pro/service/UserService.java
@@ -1,4 +1,30 @@
 package com.goatz.task_manager_pro.service;
 
+import com.goatz.task_manager_pro.dao.UserRepository;
+import com.goatz.task_manager_pro.model.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class UserService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public User createUser(User user) {
+        // Hash the password
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return userRepository.save(user);
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User getUserById(Integer id) {
+        return userRepository.findById(id).orElse(null);
+    }
 }
