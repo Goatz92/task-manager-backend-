efa3fcbc8328f44e485208304e43aceb587f007f
Goatz <Goataragua@gmail.com>
2025-12-07 17:03:31 +0200

    Feat: add authentication handling to controllers

--- src/main/java/com/goatz/task_manager_pro/controller/UserController.java
+++ src/main/java/com/goatz/task_manager_pro/controller/UserController.java
@@ -1,9 +1,12 @@
 package com.goatz.task_manager_pro.controller;
 
+import com.goatz.task_manager_pro.dto.AuthRequest;
+import com.goatz.task_manager_pro.dto.AuthResponse;
 import com.goatz.task_manager_pro.model.User;
 import com.goatz.task_manager_pro.service.UserService;
-import lombok.Getter;
+import com.goatz.task_manager_pro.util.JwtUtil;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,19 +17,26 @@ import java.util.List;
 public class UserController {
 
     private final UserService userService;
+    private final PasswordEncoder passwordEncoder;
 
-    @PostMapping
-    public User createUser(@RequestBody User user) {
+    @PostMapping("/register")
+    public User register(@RequestBody User user) {
         return userService.createUser(user);
     }
 
-    @GetMapping
-    public List<User> getAllUsers(){
-        return userService.getAllUsers();
-    }
+    @PostMapping("/login")
+    public AuthResponse login(@RequestBody AuthRequest request) {
+        User user = userService.getUserByUsername(request.getUsername());
+        if (user != null && passwordEncoder.matches(request.getPassword(), user.getPassword())) {
+            String token = JwtUtil.generateToken(user.getUsername());
+            return new AuthResponse(token);
+        } else {
+            throw new RuntimeException("Invalid credentials");
+        }
+     }
 
-    @GetMapping("/{id}")
-    public User getUserById (@PathVariable Integer id) {
-        return userService.getUserById(id);
-    }
+     @GetMapping
+    public List<User> getAllUser(){
+        return userService.getAllUsers();
+     }
 }
