197431c021d0de83c4fcad97e1db7f3b9a4b982d
Goatz <Goataragua@gmail.com>
2025-12-09 17:48:52 +0200

    Update Security config. Correct pathing.

---
+++ src/main/java/com/goatz/task_manager_pro/auth/SecurityConfig.java
@@ -0,0 +1,50 @@
+package com.goatz.task_manager_pro.auth;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/", "/index.html").permitAll()
+                        .requestMatchers("/task/users/register").permitAll()
+                        .requestMatchers(HttpMethod.GET, "task/edit/{uuid}").hasAuthority("EDIT_TASK")
+                        .requestMatchers(HttpMethod.GET, "task/delete/{uuid}").hasAuthority("EDIT_TASK")
+                        .requestMatchers(HttpMethod.POST, "/task/edit").hasAuthority("EDIT_TASK")
+                        .requestMatchers("users/tasks/**").hasAnyRole("ADMIN")
+                        .requestMatchers("users/admin/**").hasRole("ADMIN")
+                        .requestMatchers("/css/**").permitAll()
+                        .requestMatchers("/js/**").permitAll()
+                        .requestMatchers("/img/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+
+                .formLogin(formLogin -> formLogin
+                        .loginPage("/login")
+                        .defaultSuccessUrl("/users/tasks", true)
+                        .permitAll()
+                )
+                .httpBasic(Customizer.withDefaults())
+                .logout(logout -> logout
+                        .logoutSuccessUrl("/login")
+                        .invalidateHttpSession(true)
+                        .deleteCookies("JSESSIONID")
+                );
+        return http.build();
+    }
+}
