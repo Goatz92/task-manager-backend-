f2bff6a59ec065588bcbfc333a8567f6e5bfcec9
Goatz <Goataragua@gmail.com>
2025-12-09 17:36:51 +0200

    Change: Implement spring functionallity w/ correct annotations

--- src/main/java/com/goatz/task_manager_pro/model/User.java
+++ src/main/java/com/goatz/task_manager_pro/model/User.java
@@ -1,34 +1,43 @@
 package com.goatz.task_manager_pro.model;
 
+import com.goatz.task_manager_pro.model.auth.Role;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.*;
+
+@Entity
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
-@Entity
+@Builder
 @Table(name = "users")
-public class User {
+public class User extends AbstractEntity implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    private Long id;
 
-    @Column(nullable = false, unique = true)
     private String username;
-
-    @Column(nullable = false)
     private String password;
 
+    @ManyToOne
+    @JoinColumn(name = "role_id")
+    private Role role;
+
     @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", username='" + username + '\'' +
-                '}';
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+
+        Set<GrantedAuthority> grantedAuthorities = new HashSet<>();
+
+        grantedAuthorities.add(new SimpleGrantedAuthority("ROLE_" + role.getName()));
+        role.getCapabilities().forEach(capability ->
+            grantedAuthorities.add(new SimpleGrantedAuthority(capability.getName()))
+        );
+        return grantedAuthorities;
     }
 }
