e9307fb0a0c3230e31909ea0a5f31c7797ad2927
Goatz <Goataragua@gmail.com>
2025-12-09 17:34:00 +0200

    Feat: implement mapping & repo usage

--- src/main/java/com/goatz/task_manager_pro/service/UserService.java
+++ src/main/java/com/goatz/task_manager_pro/service/UserService.java
@@ -1,23 +1,46 @@
 package com.goatz.task_manager_pro.service;
 
 import com.goatz.task_manager_pro.dao.UserRepository;
+import com.goatz.task_manager_pro.dto.UserInsertDTO;
+import com.goatz.task_manager_pro.exceptions.EntityAlreadyExistsException;
+import com.goatz.task_manager_pro.mapper.Mapper;
 import com.goatz.task_manager_pro.model.User;
+import com.goatz.task_manager_pro.model.auth.Role;
+import com.goatz.task_manager_pro.repository.RoleRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final Mapper mapper;
+    private final RoleRepository roleRepository;
 
-    public User createUser(User user) {
-        // Hash the password
-        user.setPassword(passwordEncoder.encode(user.getPassword()));
-        return userRepository.save(user);
+    @Transactional(rollbackOn = Exception.class)
+    public void saveUser(UserInsertDTO userInsertDTO) throws EntityAlreadyExistsException {
+        try {
+            if (userRepository.findByUsername(userInsertDTO.getUsername()).isPresent()) {
+                throw new EntityAlreadyExistsException("User", "User with username " + userInsertDTO.getUsername() + " already exists.");
+            }
+            User user = mapper.mapToUserEntity(userInsertDTO);
+            user.setPassword(passwordEncoder.encode(user.getPassword()));   // Encrypt password
+            Role role = roleRepository.findById(userInsertDTO.getRoleId())
+                    .orElse(null);
+            user.setRole(role); // does not check for null, since UI restricts entry
+            userRepository.save(user);
+            log.info("Save succeeded for user with username={}", userInsertDTO.getUsername());
+        } catch (EntityAlreadyExistsException e) {
+            log.error("Save failed for user with username={}. User already exists", userInsertDTO.getUsername(), e);
+            throw e;
+        }
     }
 
     public List<User> getAllUsers() {
